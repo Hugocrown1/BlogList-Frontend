ec3266635273aec6daa21afbae6805db6710426e
Hugo Corona <hugo.corona.r@gmail.com>
2023-08-01 13:38:08 -0700

    cypress login form test

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,11 +1,17 @@
 describe('Blog app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000')
+  })
+  it('front page can be opened', function() {
     cy.contains('blogs')
   })
 
   it('login form can be opened', function() {
-    cy.visit('http://localhost:3000')
     cy.contains('login').click()
+    cy.get('#username').type('daedalus')
+    cy.get('#password').type('secretito')
+    cy.get('#login-button').click()
+
+    cy.contains('Jon logged in')
   })
 })
